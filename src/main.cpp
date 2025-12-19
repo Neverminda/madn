8466da519d47206eccb9559d0f37da99e7145365
Neverminda <t.h.schmitt@web.de>
2025-12-19 22:35:52 +0100

    - run 1000000 games - fix data type overrun

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ int cpuCount() {
 }
 
 int main() {
-    const int runs = 10000;
+    const int runs = 1000000;
 	const int cpu_count = cpuCount();
     auto start = std::chrono::steady_clock::now();
 
@@ -29,11 +29,11 @@ int main() {
     auto end = std::chrono::steady_clock::now();
     auto time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 
-	int colWidth = 10;
+	int colWidth = 15;
 	std::println("cpuCount      {:>{}}", cpu_count, colWidth);
     std::println("time          {:>{}}", time, colWidth);
 	std::println("total_games   {:>{}}", stats.games, colWidth);
     std::println("total_moves   {:>{}}", stats.moves, colWidth);
     std::println("moves_per_game{:>{}}", (double)stats.moves/runs, colWidth);
-    std::println("storage_size  {:>{}} bytes", stats.moves * stats.state_size, colWidth);
+    std::println("storage_size  {:>{}} bytes", (long)stats.moves * stats.state_size, colWidth);
 }
